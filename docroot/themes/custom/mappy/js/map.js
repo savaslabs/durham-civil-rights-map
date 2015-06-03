cb83c671d871cbc6e8440697c02b84851f24d4a2
Anne Tomasevich <annetvich@gmail.com>
2015-06-03 15:36:51 -0400

    Add leaflet.js and map.js [WIP]

---
+++ docroot/themes/custom/mappy/js/map.js
@@ -0,0 +1,37 @@
+(function ($) {
+    // Add basemap.
+    var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    });
+
+    // Create map and set center and zoom.
+    var map = L.map('map', {
+        scrollWheelZoom: false,
+        center: [35.9908385, -78.9005222],
+        zoom: 12
+    });
+
+    // Add basemap to map.
+    map.addLayer(baseLayer);
+
+    // Add points.
+    // TODO: get popups working.
+    function addDataToMap(data, map) {
+        var dataLayer = L.geoJson(data, {
+            onEachFeature: function(feature, layer) {
+                var popupText = feature.properties.name;
+                layer.bindPopup(popupText);
+            }
+        });
+        dataLayer.addTo(map);
+    }
+
+    // TODO: what should this path be?
+    $.getJSON('/pmp/docroot/points', function(data) {
+        addDataToMap(data, map);
+    });
+
+    // TODO: this path too.
+    L.Icon.Default.imagePath = '/pmp/docroot/themes/custom/mappy/images/leaflet';
+
+})(jQuery);
