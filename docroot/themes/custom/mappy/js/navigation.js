89e9f1933f2851d71db6515312e560fc9cb39202
Anne Tomasevich <annetvich@gmail.com>
2015-06-14 11:50:09 -0400

    Refine navigation styles

--- docroot/themes/custom/mappy/js/navigation.js
+++ docroot/themes/custom/mappy/js/navigation.js
@@ -5,7 +5,7 @@
     menuToggle.on('click', function(e) {
         e.preventDefault();
         $('#js-navigation-menu').slideToggle(function(){
-            if(('#js-navigation-menu').is(':hidden')) {
+            if($('#js-navigation-menu').is(':hidden')) {
                 $('#js-navigation-menu').removeAttr('style');
             }
         });
