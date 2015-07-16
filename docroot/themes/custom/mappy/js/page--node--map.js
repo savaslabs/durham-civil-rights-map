c79822698b09cc52347318f57ac155cc2740fc9c
Tim Stallmann <tim@stallmann.mayfirst.org>
2015-07-16 16:44:47 -0400

    Just display node point on node page map.

--- docroot/themes/custom/mappy/js/page--node--map.js
+++ docroot/themes/custom/mappy/js/page--node--map.js
@@ -1,56 +1,37 @@
-(function ($) {
-    // Add basemap.
-    var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-    });
+if (nodePoint) {
+    (function ($) {
+        // Add basemap.
+        var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+        });
 
-    // Create map and set center and zoom.
-    var map = L.map('map', {
-        zoomControl: false,
-        scrollWheelZoom: false,
-        center: [35.9908385, -78.9005222],
-        zoom: 12
-    });
+        // Create map and set center and zoom.
+        var map = L.map('map', {
+            zoomControl: false,
+            scrollWheelZoom: false,
+            center: nodePoint,
+            zoom: 14
+        });
 
-    // Add basemap to map.
-    map.addLayer(baseLayer);
+        // Add basemap to map.
+        map.addLayer(baseLayer);
 
-    // Add points.
-    function addDataToMap(data, map, icon) {
-        var dataLayer = L.geoJson(data, {
-            pointToLayer: function(feature, latLng) {
-                return L.marker(latLng, {icon: icon}).addTo(map);
-            },
-            // Only add marker for current node.
-            onEachFeature: function(feature, layer) {
-                var popupText = feature.properties.name;
-                if (feature.properties.nid == drupalSettings.mappy.nodepageMap.nid) {
-                    layer.bindPopup(popupText).openPopup();
-                } else {
-                    layer.bindPopup(popupText);
-                }
+        // Set path to marker image.
+        L.Icon.Default.imagePath = '/themes/custom/mappy/images/leaflet';
+        var pmpIcon = L.Icon.extend({
+            options: {
+                iconUrl: '/themes/custom/mappy/images/leaflet/pmp-marker-icon-green.png',
+                iconRetinaUrl: '/themes/custom/mappy/images/leaflet/pmp-marker-icon-green-2x.png',
+                iconSize: [25, 41],
+                iconAnchor: [13, 40],
+                popupAnchor: [1, -46]
             }
         });
-        dataLayer.addTo(map);
-    }
 
-    // Set path to marker image.
-    L.Icon.Default.imagePath = '/themes/custom/mappy/images/leaflet';
-    var pmpIcon = L.Icon.extend({
-        options: {
-            iconUrl: '/themes/custom/mappy/images/leaflet/pmp-marker-icon-green.png',
-            iconRetinaUrl: '/themes/custom/mappy/images/leaflet/pmp-marker-icon-green-2x.png',
-            iconSize: [25, 41],
-            iconAnchor: [13, 40],
-            popupAnchor: [1, -46]
-        }
-    });
+        L.marker(nodePoint, {icon: new pmpIcon}).addTo(map).bindPopup(nodeTitle);;
 
-    $.getJSON('/points', function(data) {
-        addDataToMap(data, map, new pmpIcon);
-    });
+        // Add zoom controls in bottom right of map.
+        new L.Control.Zoom({position: 'bottomright'}).addTo(map);
 
-    // Add zoom controls in bottom right of map.
-    new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
-
-})(jQuery);
+    })(jQuery);
+}
